150e5851032945681ddf0eb07689af00fc40e977
Saumya <saumyasindhu75@gmail.com>
2026-04-17 20:05:33 +0530

    added spotify

---
+++ src/components/windows/Spotify.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import MacWindow from './MacWindow';
+import './spotify.scss';
+
+const Spotify = () => {
+  return (
+    <MacWindow>
+      <div className="spotify-window">
+        <iframe 
+          data-testid="embed-iframe"
+          style={{borderRadius: "12px"}}
+          src="https://open.spotify.com/embed/playlist/6Ym1bv4FPXAeIeEfYsIcnv?utm_source=generator"
+          width="100%"
+          height="352"
+          frameBorder="0"
+          allowfullscreen=""
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          loading="lazy"
+        ></iframe>
+      </div>
+    </MacWindow>
+  );
+}
+
+export default Spotify
